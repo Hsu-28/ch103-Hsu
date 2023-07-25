ea5072826e5397e3d08da6431816c19721f9ee7b
Hsu-28 <svm3g6m4s@gmail.com>
2023-07-26 01:32:21 +0800

    idnight

---
+++ js/member.js
@@ -0,0 +1,23 @@
+const article = document.querySelector(".memberOverflow");
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const firtform = document.getElementById("form1");
+const secondform = document.getElementById("form2");
+const container = document.querySelector(".container-member")
+
+signInBtn.addEventListener("click", ()=>{
+    container.classList.remove("right-panel-active")   
+})
+
+signUpBtn.addEventListener("click", ()=>{
+    container.classList.add("right-panel-active")   
+})
+document.getElementById("memberLog").addEventListener("click", function() {
+article.style.display = "grid";
+});
+
+document.addEventListener("click", (event) => {
+    if (!event.target.closest(".container-member") && event.target == article) {
+        article.style.display = "none";
+    }
+});
